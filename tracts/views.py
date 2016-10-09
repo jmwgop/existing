268f57946ee68503e5228b8931da013db1b8f8c2
John Michael <jmwgop@gmail.com>
2016-10-08 20:05:28 -0500

    created account list view

--- tracts/views.py
+++ tracts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+from .models import Tracts
+
+class TractList(ListView):
+    model = Tracts
+    template_name = 'tracts/tract_list.html'
+    context_object_name = 'tracts'
+
+    def get_queryset(self):
+        tract_list = Tracts.objects.filter(owner=self.request.user)
+        return tract_list
+
+    @login_required(login_url='/accounts/login/')
+    def dispatch(self, *args, **kwargs):
+        return super(TractList, self).dispatch(*args, **kwargs)
